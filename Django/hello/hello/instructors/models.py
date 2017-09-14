06f64c1bd333767c6de8920b146f6796f89f9237
dukon <smaksim@bk.ru>
2017-09-14 18:00:10 +0600

    14.09

--- Django/hello/hello/instructors/models.py
+++ Django/hello/hello/instructors/models.py
@@ -2,7 +2,32 @@ from django.db import models
 
 # Create your models here.
 
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+        
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+        
 class Instructor(models.Model):
-    name = models.CharField(max_length=64)
-    surname = models.CharField(max_length=64)
-    course = models.CharField(max_length=64)
+    name = models.CharField(verbose_name=u'Instructor Name', max_length=255, help_text="This is name")
+    surname = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True)
+    #phone = models.CharField(max_length=15, null=True, blank=True)
+    courses = models.ManyToManyField(Course)
+    #course = models.CharField(max_length=255)
+    #slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=True)
+    
+    position = models.ForeignKey(Position)
+    
+    def __str__(self):
+        return self.name
